90b98a001cdef1b61fde12a444ede76a38f269ea
Rob Hoelz <rob@hoelz.ro>
2012-06-22 18:42:40 -0500

    initial commit

---
+++ lib/MooseX/Getopt/Explicit.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package MooseX::Getopt::Explicit;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of MooseX::Getopt::Explicit
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
