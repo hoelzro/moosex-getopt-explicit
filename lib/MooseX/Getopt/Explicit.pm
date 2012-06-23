546fea987c87afc83de2088156e1d070c064bce9
Rob Hoelz <rob@hoelz.ro>
2012-06-22 19:50:26 -0500

    Add documentation

--- lib/MooseX/Getopt/Explicit.pm
+++ lib/MooseX/Getopt/Explicit.pm
@@ -20,14 +20,32 @@ around _compute_getopt_attrs => sub {
 
 __END__
 
-# ABSTRACT: A short description of MooseX::Getopt::Explicit
+# ABSTRACT: MooseX::Getopt, but without implicit option generation
 
 =head1 SYNOPSIS
 
+  use Moose;
+  with 'MooseX::Getopt::Explicit';
+
+  has foo => (
+    is => 'rw',
+  ); # Does *not* cause a Getopt option to be generated!
+
+  has bar => (
+    is     => 'ro'
+    isa    => 'Str',
+    traits => ['Getopt'],
+  ); # *Does* cause a Getopt option to be generated!
+
 =head1 DESCRIPTION
 
-=head1 FUNCTIONS
+L<MooseX::Getopt> is nice, but I don't care for how it creates a command
+line option for every attribute in my classes unless explicitly overridden.
+So this role does the opposite: it requires C<traits =E<gt> ['Getopt']> in
+order for a command line option to be generated.
 
 =head1 SEE ALSO
+
+L<MooseX::Getopt>
 
 =cut
